99d2cd77c85c26faea86f3598abc9ee5a98a8977
Bryce Fischer <bryce-fischer@idexx.com>
2023-06-29 13:45:40 -0400

    template for host

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: host</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <div className="text-center">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp"
+        className="rounded-full w-32 mb-4 mx-auto"
+        alt="Avatar"
+      />
+      <h5 className="text-xl font-medium leading-tight mb-2">John Doe</h5>
+      <p className="text-gray-500">Web designer</p>
+    </div>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
