c5188c1dd5575cefd223f0db363f681b7c83a175
Bryce Fischer <bryce-fischer@idexx.com>
2023-06-29 13:37:25 -0400

    Added header and footer

--- shared-components/src/App.tsx
+++ shared-components/src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: shared-components</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home page Content</div>
+    <Footer />
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
